2ae526c8220f299bbbc2f0258e308fdbe737f543
Somashekar G. Achar <somashekar@mobilecrmplus.us>
2019-05-06 17:54:23 +0530

    awscli

--- profile/aspnetcore-defaults.sh
+++ profile/aspnetcore-defaults.sh
@@ -5,4 +5,4 @@ export LIBRARY_PATH="$LIBRARY_PATH:$HOME/.apt/usr/lib/x86_64-linux-gnu:$HOME/.ap
 export INCLUDE_PATH="$INCLUDE_PATH:$HOME/.apt/usr/include"
 export CPATH="$INCLUDE_PATH"
 export CPPPATH="$INCLUDE_PATH"
-export apt_CONFIG_PATH="$apt_CONFIG_PATH:$HOME/.apt/usr/lib/x86_64-linux-gnu/aptconfig:$HOME/.apt/usr/lib/i386-linux-gnu/aptconfig:$HOME/.apt/usr/lib/aptconfig"
+export APT_CONFIG_PATH="$APT_CONFIG_PATH:$HOME/.apt/usr/lib/x86_64-linux-gnu/aptconfig:$HOME/.apt/usr/lib/i386-linux-gnu/aptconfig:$HOME/.apt/usr/lib/aptconfig"
